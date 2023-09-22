06a7d6d9c1ec9605c91360c002e821f7664de7e8
Jaip <145761147+Jaip-Coding@users.noreply.github.com>
2023-09-22 20:14:12 +0200

    Add files via upload

---
+++ none.py
@@ -0,0 +1,152 @@
+################################
+# NONE v1.0 Compiler in Python #
+################################
+
+import re
+
+TOKENS = "abcdefghijklmnopqrstuvwxyz"
+TOKENS_CAPS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+SPECIAL_TOKENS = '.!?":()/%'
+
+USER_INPUT = input()
+
+USER_TOKENS = re.split(r'(\d|\D)', USER_INPUT)
+
+for token in USER_TOKENS:
+    USER_TOKENS.remove("")
+
+INDEX = 0
+LAST_TOKEN = None
+TOKENS_LIST = [*TOKENS]
+TOKENS_CAPS_LIST = [*TOKENS_CAPS]
+SPECIAL_TOKENS_LIST = [*SPECIAL_TOKENS]
+IS_NUMBER = False
+NUMBER_INDEX = 0
+IS_SPECIAL = False
+SPECIAL_INDEX = 0
+OUTPUT = ""
+
+for token in USER_TOKENS:
+    if IS_NUMBER:
+        if LAST_TOKEN == "+":
+            if token == "+":
+                NUMBER_INDEX += 1
+                LAST_TOKEN = None
+            elif token == "V" or token == "v":
+                NUMBER_INDEX += 5
+                LAST_TOKEN = None
+        elif LAST_TOKEN == "-":
+            if token == "-":
+                NUMBER_INDEX -= 1
+                LAST_TOKEN = None
+            elif token == "V" or token == "v":
+                NUMBER_INDEX -= 5
+                LAST_TOKEN = None
+        elif LAST_TOKEN is None:
+            #print("TEST")
+            #print(token)
+            if token == "+":
+                LAST_TOKEN = "+"
+            elif token == "-":
+                LAST_TOKEN = "-"
+            elif token == ")":
+                LAST_TOKEN = None
+                OUTPUT += str(NUMBER_INDEX)
+                NUMBER_INDEX = 0
+                IS_NUMBER = False
+        #print(NUMBER_INDEX)
+        continue
+    
+    if IS_SPECIAL:
+        if LAST_TOKEN == "+":
+            if token == "+":
+                SPECIAL_INDEX += 1
+                LAST_TOKEN = None
+            elif token == "V" or token == "v":
+                SPECIAL_INDEX += 5
+                LAST_TOKEN = None
+        elif LAST_TOKEN == "-":
+            if token == "-":
+                SPECIAL_INDEX -= 1
+                LAST_TOKEN = None
+            elif token == "V" or token == "v":
+                SPECIAL_INDEX -= 5
+                LAST_TOKEN = None
+        elif LAST_TOKEN is None:
+            if token == "+":
+                LAST_TOKEN = "+"
+            elif token == "-":
+                LAST_TOKEN = "-"
+            elif token == ")":
+                LAST_TOKEN = None
+                OUTPUT += SPECIAL_TOKENS_LIST[SPECIAL_INDEX - 1]
+                SPECIAL_INDEX = 0
+                IS_SPECIAL = False
+        continue
+    
+    if LAST_TOKEN is not None:
+        if LAST_TOKEN == "+":
+            if token == "+":
+                INDEX += 1
+                LAST_TOKEN = None
+            elif token == "V" or token == "v":
+                INDEX += 5
+                LAST_TOKEN = None
+            elif token == "X" or token == "x":
+                INDEX += 10
+                LAST_TOKEN = None
+            elif token == "T" or token == "t":
+                INDEX += 20
+                LAST_TOKEN = None
+        elif LAST_TOKEN == "-":
+            if token == "-":
+                INDEX -= 1
+                LAST_TOKEN = None
+            elif token == "V" or token == "v":
+                INDEX -= 5
+                LAST_TOKEN = None
+            elif token == "X" or token == "x":
+                INDEX -= 10
+                LAST_TOKEN = None
+            elif token == "T" or token == "t":
+                INDEX -= 20
+                LAST_TOKEN = None
+        elif LAST_TOKEN == "^":
+            if token == "P" or token == "p":
+                OUTPUT += TOKENS_CAPS_LIST[INDEX - 1]
+            LAST_TOKEN = None
+        elif LAST_TOKEN == "n":
+            if token == "(":
+                LAST_TOKEN = "n("
+        elif LAST_TOKEN == "n(":
+            IS_NUMBER = True
+            if token == "+":
+                LAST_TOKEN = "+"
+        elif LAST_TOKEN == "s":
+            if token == "(":
+                LAST_TOKEN = "s("
+        elif LAST_TOKEN == "s(":
+            IS_SPECIAL = True
+            if token == "+":
+                LAST_TOKEN = "+"
+    elif LAST_TOKEN is None:
+        if token == "+":
+            LAST_TOKEN = "+"
+        elif token == "-":
+            LAST_TOKEN = "-"
+        elif token == "P" or token == "p":
+            OUTPUT += TOKENS_LIST[INDEX - 1]
+        elif token == "^":
+            LAST_TOKEN = "^"
+        elif token == "n" or token == "N":
+            LAST_TOKEN = "n"
+        elif token == "s" or token == "S":
+            LAST_TOKEN = "s"
+        elif token == "_":
+            OUTPUT += " "
+            LAST_TOKEN = None
+        elif token == "c":
+            INDEX = 0
+            LAST_TOKEN = None
+
+print(OUTPUT)
---
+++ none_generator.py
@@ -0,0 +1,396 @@
+######################
+# NONE 1.0 Generator #
+######################
+
+import re
+
+TOKENS = "abcdefghijklmnopqrstuvwxyz"
+TOKENS_CAPS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+USER_INPUT = input()
+
+USER_TOKENS = re.split(r'(\d|\D)', USER_INPUT)
+
+for token in USER_TOKENS:
+    USER_TOKENS.remove("")
+
+OUTPUT = ""
+INDEX = 0
+
+def Clear(INDEX):
+    OUTPUT_C = ""
+    if POS - INDEX > 7:
+        OUTPUT_C += "+x"
+        INDEX += 10
+        if POS - INDEX > 3:
+            OUTPUT_C += "+v"
+            INDEX += 5
+            if POS - INDEX > 0:
+                for i in range(0, POS - INDEX):
+                    OUTPUT_C += "++"
+                    INDEX += 1
+            elif POS - INDEX < 0:
+                for i in range(0, abs(POS - INDEX)):
+                    OUTPUT_C += "--"
+                    INDEX -= 1
+        elif POS - INDEX < -3:
+            OUTPUT_C += "-v"
+            INDEX -= 5
+            if POS - INDEX > 0:
+                for i in range(0, POS - INDEX):
+                    OUTPUT_C += "++"
+                    INDEX += 1
+            elif POS - INDEX < 0:
+                for i in range(0, abs(POS - INDEX)):
+                    OUTPUT_C += "--"
+                    INDEX -= 1
+        elif POS - INDEX > 0:
+            for i in range(0, POS - INDEX):
+                OUTPUT_C += "++"
+                INDEX += 1
+        elif POS - INDEX < 0:
+            for i in range(0, abs(POS - INDEX)):
+                OUTPUT_C += "--"
+                INDEX -= 1
+    elif POS - INDEX > 3:
+        OUTPUT_C += "+v"
+        INDEX += 5
+        if POS - INDEX > 0:
+            for i in range(0, POS - INDEX):
+                OUTPUT_C += "++"
+                INDEX += 1
+        elif POS - INDEX < 0:
+            for i in range(0, abs(POS - INDEX)):
+                OUTPUT_C += "--"
+                INDEX -= 1
+    elif POS - INDEX > 0:
+        for i in range(0, POS - INDEX):
+            OUTPUT_C += "++"
+            INDEX += 1
+    
+    return [OUTPUT_C, INDEX]
+
+
+for token in USER_TOKENS:
+    if token == "0":
+        OUTPUT += "n(++--)"
+    elif token == "1":
+        OUTPUT += "n(++)"
+    elif token == "2":
+        OUTPUT += "n(++++)"
+    elif token == "3":
+        OUTPUT += "n(++++++)"
+    elif token == "4":
+        OUTPUT += "n(+v--)"
+    elif token == "5":
+        OUTPUT += "n(+v)"
+    elif token == "6":
+        OUTPUT += "n(+v++)"
+    elif token == "7":
+        OUTPUT += "n(+v++++)"
+    elif token == "8":
+        OUTPUT += "n(+v++++++)"
+    elif token == "9":
+        OUTPUT += "n(+v++++++++)"
+    elif token == ".":
+        OUTPUT += "s(++)"
+    elif token == "!":
+        OUTPUT += "s(++++)"
+    elif token == '?':
+        OUTPUT += "s(++++++)"
+    elif token == '"':
+        OUTPUT += "s(+v--)"
+    elif token == ":":
+        OUTPUT += "s(+v)"
+    elif token == "(":
+        OUTPUT += "s(+v++)"
+    elif token == ")":
+        OUTPUT += "s(+v++++)"
+    elif token == "/":
+        OUTPUT += "s(+v++++++)"
+    elif token == "%":
+        OUTPUT += "s(+v++++++++)"
+    elif token in TOKENS:
+        POS = TOKENS.index(token) + 1
+        if POS - INDEX >= 20 or POS - INDEX > 17:
+            OUTPUT += "+t"
+            INDEX += 20
+            if POS - INDEX > 3:
+                OUTPUT += "+v"
+                INDEX += 5
+                if POS - INDEX > 0:
+                    for i in range(0, POS - INDEX):
+                        OUTPUT += "++"
+                        INDEX += 1
+                elif POS - INDEX < 0:
+                    for i in range(0, abs(POS - INDEX)):
+                        OUTPUT += "--"
+                        INDEX -= 1
+            elif POS - INDEX < -3:
+                OUTPUT += "-v"
+                INDEX -= 5
+                if POS - INDEX > 0:
+                    for i in range(0, POS - INDEX):
+                        OUTPUT += "++"
+                        INDEX += 1
+                elif POS - INDEX < 0:
+                    for i in range(0, abs(POS - INDEX)):
+                        OUTPUT += "--"
+                        INDEX -= 1
+            elif POS - INDEX > 0:
+                for i in range(0, POS - INDEX):
+                    OUTPUT += "++"
+                    INDEX += 1
+            elif POS - INDEX < 0:
+                for i in range(0, abs(POS - INDEX)):
+                    OUTPUT += "--"
+                    INDEX -= 1
+        elif POS - INDEX > 7:
+            OUTPUT += "+x"
+            INDEX += 10
+            if POS - INDEX > 3:
+                OUTPUT += "+v"
+                INDEX += 5
+                if POS - INDEX > 0:
+                    for i in range(0, POS - INDEX):
+                        OUTPUT += "++"
+                        INDEX += 1
+                elif POS - INDEX < 0:
+                    for i in range(0, abs(POS - INDEX)):
+                        OUTPUT += "--"
+                        INDEX -= 1
+            elif POS - INDEX < -3:
+                OUTPUT += "-v"
+                INDEX -= 5
+                if POS - INDEX > 0:
+                    for i in range(0, POS - INDEX):
+                        OUTPUT += "++"
+                        INDEX += 1
+                elif POS - INDEX < 0:
+                    for i in range(0, abs(POS - INDEX)):
+                        OUTPUT += "--"
+                        INDEX -= 1
+            elif POS - INDEX > 0:
+                for i in range(0, POS - INDEX):
+                    OUTPUT += "++"
+                    INDEX += 1
+            elif POS - INDEX < 0:
+                for i in range(0, abs(POS - INDEX)):
+                    OUTPUT += "--"
+                    INDEX -= 1
+        elif POS - INDEX > 3:
+            OUTPUT += "+v"
+            INDEX += 5
+            if POS - INDEX > 0:
+                for i in range(0, POS - INDEX):
+                    OUTPUT += "++"
+                    INDEX += 1
+            elif POS - INDEX < 0:
+                for i in range(0, abs(POS - INDEX)):
+                    OUTPUT += "--"
+                    INDEX -= 1
+        elif POS - INDEX > 0:
+            for i in range(0, POS - INDEX):
+                OUTPUT += "++"
+                INDEX += 1
+        elif POS - 0 > POS - INDEX:
+            OUTPUT += "c"
+            INDEX = 0
+            clear_func = Clear(INDEX)
+            OUTPUT += str(Clear(INDEX)[0])
+            INDEX = clear_func[1]
+        elif POS - INDEX < -7:
+            OUTPUT += "-x"
+            INDEX -= 10
+            if POS - INDEX > 3:
+                OUTPUT += "+v"
+                INDEX += 5
+                if POS - INDEX > 0:
+                    for i in range(0, POS - INDEX):
+                        OUTPUT += "++"
+                        INDEX += 1
+                elif POS - INDEX < 0:
+                    for i in range(0, abs(POS - INDEX)):
+                        OUTPUT += "--"
+                        INDEX -= 1
+            elif POS - INDEX < -3:
+                OUTPUT += "-v"
+                INDEX -= 5
+                if POS - INDEX > 0:
+                    for i in range(0, POS - INDEX):
+                        OUTPUT += "++"
+                        INDEX += 1
+                elif POS - INDEX < 0:
+                    for i in range(0, abs(POS - INDEX)):
+                        OUTPUT += "--"
+                        INDEX -= 1
+            elif POS - INDEX > 0:
+                for i in range(0, POS - INDEX):
+                    OUTPUT += "++"
+                    INDEX += 1
+            elif POS - INDEX < 0:
+                for i in range(0, abs(POS - INDEX)):
+                    OUTPUT += "--"
+                    INDEX -= 1
+        elif POS - INDEX < -3:
+            OUTPUT += "-v"
+            INDEX -= 5
+            if POS - INDEX > 0:
+                for i in range(0, POS - INDEX):
+                    OUTPUT += "++"
+                    INDEX += 1
+            elif POS - INDEX < 0:
+                for i in range(0, abs(POS - INDEX)):
+                    OUTPUT += "--"
+                    INDEX -= 1
+        elif POS - INDEX < 0:
+            for i in range(0, POS - INDEX):
+                OUTPUT += "--"
+                INDEX -= 1
+        
+        OUTPUT += "p"
+        
+    elif token in TOKENS_CAPS:
+        POS = TOKENS_CAPS.index(token) + 1
+        if POS - INDEX >= 20 or POS - INDEX > 17:
+            OUTPUT += "+t"
+            INDEX += 20
+            if POS - INDEX > 3:
+                OUTPUT += "+v"
+                INDEX += 5
+                if POS - INDEX > 0:
+                    for i in range(0, POS - INDEX):
+                        OUTPUT += "++"
+                        INDEX += 1
+                elif POS - INDEX < 0:
+                    for i in range(0, abs(POS - INDEX)):
+                        OUTPUT += "--"
+                        INDEX -= 1
+            elif POS - INDEX < -3:
+                OUTPUT += "-v"
+                INDEX -= 5
+                if POS - INDEX > 0:
+                    for i in range(0, POS - INDEX):
+                        OUTPUT += "++"
+                        INDEX += 1
+                elif POS - INDEX < 0:
+                    for i in range(0, abs(POS - INDEX)):
+                        OUTPUT += "--"
+                        INDEX -= 1
+            elif POS - INDEX > 0:
+                for i in range(0, POS - INDEX):
+                    OUTPUT += "++"
+                    INDEX += 1
+            elif POS - INDEX < 0:
+                for i in range(0, abs(POS - INDEX)):
+                    OUTPUT += "--"
+                    INDEX -= 1
+        elif POS - INDEX > 7:
+            OUTPUT += "+x"
+            INDEX += 10
+            if POS - INDEX > 3:
+                OUTPUT += "+v"
+                INDEX += 5
+                if POS - INDEX > 0:
+                    for i in range(0, POS - INDEX):
+                        OUTPUT += "++"
+                        INDEX += 1
+                elif POS - INDEX < 0:
+                    for i in range(0, abs(POS - INDEX)):
+                        OUTPUT += "--"
+                        INDEX -= 1
+            elif POS - INDEX < -3:
+                OUTPUT += "-v"
+                INDEX -= 5
+                if POS - INDEX > 0:
+                    for i in range(0, POS - INDEX):
+                        OUTPUT += "++"
+                        INDEX += 1
+                elif POS - INDEX < 0:
+                    for i in range(0, abs(POS - INDEX)):
+                        OUTPUT += "--"
+                        INDEX -= 1
+            elif POS - INDEX > 0:
+                for i in range(0, POS - INDEX):
+                    OUTPUT += "++"
+                    INDEX += 1
+            elif POS - INDEX < 0:
+                for i in range(0, abs(POS - INDEX)):
+                    OUTPUT += "--"
+                    INDEX -= 1
+        elif POS - INDEX > 3:
+            OUTPUT += "+v"
+            INDEX += 5
+            if POS - INDEX > 0:
+                for i in range(0, POS - INDEX):
+                    OUTPUT += "++"
+                    INDEX += 1
+            elif POS - INDEX < 0:
+                for i in range(0, abs(POS - INDEX)):
+                    OUTPUT += "--"
+                    INDEX -= 1
+        elif POS - INDEX > 0:
+            for i in range(0, POS - INDEX):
+                OUTPUT += "++"
+                INDEX += 1
+        elif POS - 0 > POS - INDEX:
+            OUTPUT += "c"
+            INDEX = 0
+            clear_func = Clear(INDEX)
+            OUTPUT += str(Clear(INDEX)[0])
+            INDEX = clear_func[1]
+        elif POS - INDEX < -7:
+            OUTPUT += "-x"
+            INDEX -= 10
+            if POS - INDEX > 3:
+                OUTPUT += "+v"
+                INDEX += 5
+                if POS - INDEX > 0:
+                    for i in range(0, POS - INDEX):
+                        OUTPUT += "++"
+                        INDEX += 1
+                elif POS - INDEX < 0:
+                    for i in range(0, abs(POS - INDEX)):
+                        OUTPUT += "--"
+                        INDEX -= 1
+            elif POS - INDEX < -3:
+                OUTPUT += "-v"
+                INDEX -= 5
+                if POS - INDEX > 0:
+                    for i in range(0, POS - INDEX):
+                        OUTPUT += "++"
+                        INDEX += 1
+                elif POS - INDEX < 0:
+                    for i in range(0, abs(POS - INDEX)):
+                        OUTPUT += "--"
+                        INDEX -= 1
+            elif POS - INDEX > 0:
+                for i in range(0, POS - INDEX):
+                    OUTPUT += "++"
+                    INDEX += 1
+            elif POS - INDEX < 0:
+                for i in range(0, abs(POS - INDEX)):
+                    OUTPUT += "--"
+                    INDEX -= 1
+        elif POS - INDEX < -3:
+            OUTPUT += "-v"
+            INDEX -= 5
+            if POS - INDEX > 0:
+                for i in range(0, POS - INDEX):
+                    OUTPUT += "++"
+                    INDEX += 1
+            elif POS - INDEX < 0:
+                for i in range(0, abs(POS - INDEX)):
+                    OUTPUT += "--"
+                    INDEX -= 1
+        elif POS - INDEX < 0:
+            for i in range(0, POS - INDEX):
+                OUTPUT += "--"
+                INDEX -= 1
+        
+        OUTPUT += "^p"
+    
+    elif token == " ":
+        OUTPUT += "_"
+
+print(OUTPUT)
