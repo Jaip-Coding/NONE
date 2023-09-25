47a272dc4d60f33db81cc04ac7b62c2113796e22
Jaip <145761147+Jaip-Coding@users.noreply.github.com>
2023-09-25 10:20:10 +0200

    Update none.py

--- none.py
+++ none.py
@@ -6,7 +6,8 @@ import re
 
 TOKENS = "abcdefghijklmnopqrstuvwxyz"
 TOKENS_CAPS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-SPECIAL_TOKENS = '_.!?-:()@%'
+SPECIAL_TOKENS = '_.!?-:()@#'
+MATH_TOKENS = "$+-*/=%^<>"
 
 USER_INPUT = input()
 
@@ -20,10 +21,13 @@ LAST_TOKEN = None
 TOKENS_LIST = [*TOKENS]
 TOKENS_CAPS_LIST = [*TOKENS_CAPS]
 SPECIAL_TOKENS_LIST = [*SPECIAL_TOKENS]
+MATH_TOKENS_LIST = [*MATH_TOKENS]
 IS_NUMBER = False
 NUMBER_INDEX = 0
 IS_SPECIAL = False
 SPECIAL_INDEX = 0
+IS_MATH = False
+MATH_INDEX = 0
 OUTPUT = ""
 
 for token in USER_TOKENS:
@@ -84,6 +88,33 @@ for token in USER_TOKENS:
                 IS_SPECIAL = False
         continue
     
+    if IS_MATH:
+        if LAST_TOKEN == "+":
+            if token == "+":
+                MATH_INDEX += 1
+                LAST_TOKEN = None
+            elif token == "V" or token == "v":
+                MATH_INDEX += 5
+                LAST_TOKEN = None
+        elif LAST_TOKEN == "-":
+            if token == "-":
+                MATH_INDEX -= 1
+                LAST_TOKEN = None
+            elif token == "V" or token == "v":
+                MATH_INDEX -= 5
+                LAST_TOKEN = None
+        elif LAST_TOKEN is None:
+            if token == "+":
+                LAST_TOKEN = "+"
+            elif token == "-":
+                LAST_TOKEN = "-"
+            elif token == ")":
+                LAST_TOKEN = None
+                OUTPUT += MATH_TOKENS_LIST[MATH_INDEX]
+                MATH_INDEX = 0
+                IS_MATH = False
+        continue
+    
     if LAST_TOKEN is not None:
         if LAST_TOKEN == "+":
             if token == "+":
@@ -129,6 +160,13 @@ for token in USER_TOKENS:
             IS_SPECIAL = True
             if token == "+":
                 LAST_TOKEN = "+"
+        elif LAST_TOKEN == "m":
+            if token == "(":
+                LAST_TOKEN = "m("
+        elif LAST_TOKEN == "m(":
+            IS_MATH = True
+            if token == "+":
+                LAST_TOKEN = "+"
     elif LAST_TOKEN is None:
         if token == "+":
             LAST_TOKEN = "+"
@@ -142,6 +180,8 @@ for token in USER_TOKENS:
             LAST_TOKEN = "n"
         elif token == "s" or token == "S":
             LAST_TOKEN = "s"
+        elif token == "m" or token == "M":
+            LAST_TOKEN = "m"
         elif token == "_":
             OUTPUT += " "
             LAST_TOKEN = None
